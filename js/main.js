b8e2a42179c1b69afe69b133a0fd5519b3dccbc9
souparno majumder <souparno.majumder@gmail.com>
2015-02-13 23:18:16 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,62 +1,37 @@
-(function(window, undefined) {
-    'use strict';
+var Ajax = (function () {
+  'use strict';
 
-    var // Localise globals
-        //document = window.document,
-        $ = window.$,
-        CIS = window.CIS = window.CIS || {};
+  return {
 
-    CIS.Ajax = {
-        /*
-         * Hold the last context that was set by the request.
-         * In most case, it will refer to a DOM element that trigger the request.
-         * Best use for debugging a response from CIS.Ajax.request function.
-         */
-        lastContext: undefined,
-        /**
-         * Perform an Ajax request
-         * The response will be handled by CI.Ajax.response function
-         * url: the URL to which the request is sent
-         * settings: settings for $.ajax() function (optional)
-         */
-        request: function(url, settings) {
-            settings = settings || {};
+    request: function (url, settings) {
+      var self = this;
 
-            settings = $.extend({
-                async: true,
-                cache: false,
-                dataType: 'json',
-                type: 'GET',
-                context:{},
-                success: function(data) {
-                    CIS.Ajax.response.call(settings.context, data);
-                }
-            }, settings);
-            $.ajax(url, settings);
-        },
-        /**
-         * Handle JSON data responded from CI.Ajax.request function
-         * data: JSON data
-         *      contains array of scripts to be executed
-         */
-        response: function(data) {
-            data = data || {};
-            var context = this;
-            CIS.Ajax.lastContext = context;
-
-            if (typeof data.scripts === 'undefined') {
-                return;
-            }
-
-            // Execute all scripts from the response
-            for (var i = 0, length = data.scripts.length; i < length; i++) {
-                try {
-                    eval(data.scripts[i]); 
-                } catch(ex) {
-                    console.log(ex);
-                }
-            }
+      settings = settings || {};
+      settings = $.extend({
+        async: true,
+        cache: false,
+        dataType: 'json',
+        type: 'GET',
+        context: {},
+        success: function (data) {
+          self.response.call(settings, data);
         }
-    };
+      }, settings);
+      $.ajax(url, settings);
+    },
 
-})(window);
+    response: function (data) {
+      var context = this.context,
+        scripts = data.scripts,
+        key;
+
+      for (key in scripts) {
+        try {
+          eval(scripts[key]);
+        } catch (ex) {
+          console.log(ex);
+        }
+      }
+    }
+  };
+}());
